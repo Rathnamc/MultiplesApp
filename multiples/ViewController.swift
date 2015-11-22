aec7f500ab9ffd1d0f4b446e96b856e577045426
Chris Rathnam <Rathnam.C@gmail.com>
2015-11-22 03:38:14 -0500

    Multiples App Completed

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     //Variables
-    var currentMultiple = 0
+    var choosenMultiple = 0
     var updatedMultiple = 0
+    var result = 0
     var maxMultiple = 100
     
     //Outlets
@@ -20,6 +21,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var multiTxt: UITextField!
     @IBOutlet weak var addBtn: UIButton!
     @IBOutlet weak var addLbl: UILabel!
+    @IBOutlet weak var equationLbl: UILabel!
+    
+    @IBAction func onAddBtnPress(sender: UIButton!) {
+        updatedMultiple = result
+        result = choosenMultiple + updatedMultiple
+        updateEquationLbl()
+        
+        if isGameOver(){
+            restartGame()
+        }
+    }
     
     
     @IBAction func onPlayBtnPress(sender: UIButton! ){
@@ -33,10 +45,48 @@ class ViewController: UIViewController {
             multiLogo.hidden = false
             addBtn.hidden = false
             addLbl.hidden = false
+            equationLbl.hidden = false
+            
+            //Convert String input into Int
+            choosenMultiple = Int(multiTxt.text!)!
+            updatedMultiple = 0
+            result = choosenMultiple + updatedMultiple
+            updateEquationLbl()
             
         }
     }
     
+    //Restart Function Once game is Complete... Resets all Settings
+    func restartGame(){
+        choosenMultiple = 0
+        updatedMultiple = 0
+        multiTxt.text = ""
+        
+        playBtn.hidden = false
+        multiTxt.hidden = false
+        multiLogo.hidden = false
+        
+        addLbl.hidden = true
+        addBtn.hidden = true
+        equationLbl.hidden = true
+        
+        
+        
+    }
+    
+    //Check is multiplier hits max (100)
+    func isGameOver() -> Bool{
+        if result >= maxMultiple{
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    //Function to update Equation
+    func updateEquationLbl() {
+        equationLbl.text = "\(choosenMultiple) + \(updatedMultiple) = \(result)"
+    }
     
 
 }
