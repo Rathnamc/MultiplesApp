651629c3f53628566c00fb060d31797abf198775
Chris Rathnam <Rathnam.C@gmail.com>
2015-11-22 02:13:13 -0500

    Storyboard complete, Press button to play complete

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Variables
+    var currentMultiple = 0
+    var updatedMultiple = 0
+    var maxMultiple = 100
+    
+    //Outlets
+    @IBOutlet weak var multiLogo: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var multiTxt: UITextField!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var addLbl: UILabel!
+    
+    
+    @IBAction func onPlayBtnPress(sender: UIButton! ){
+        
+        //Input Validation
+        if multiTxt.text != nil && multiTxt.text != "" {
+            
+            playBtn.hidden = true
+            multiTxt.hidden = true
+            
+            multiLogo.hidden = false
+            addBtn.hidden = false
+            addLbl.hidden = false
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
 
 }
+
 
